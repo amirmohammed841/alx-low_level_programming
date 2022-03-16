8813041cecd2175af762b16357a030279bbadc04
amirmohammed841 <99578580+amirmohammed841@users.noreply.github.com>
2022-03-16 23:31:04 +0300

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,21 @@
-#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry block
+ * @void: no argument
+ * Return: 0
+ **/
 int main(void)
 {
-printf("putchar\n");
-	return 0;
+	char ermias[] = "_putchar";
 
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(ermias[c]);
+	}
+	_putchar('\n');
+	return (0);
 }
 
 
